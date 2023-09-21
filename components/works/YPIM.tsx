0edc024d0421b1eb52eaa6940ccf62d5cc633891
RickyDarwid <63650667+rickydarmawanwidjanarko@users.noreply.github.com>
2023-09-21 14:50:37 +0700

    a

--- components/works/YPIM.tsx
+++ components/works/YPIM.tsx
@@ -11,30 +11,30 @@ const YPIM = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        IT
-        <span className="text-textOrange tracking-wide">@YPIM</span>
+        {'IT'}
+        <span className="text-textOrange tracking-wide">{'@YPIM'}</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
-        Nov 2019 - present
+        {'Nov 2019'} - present
       </p>
       <ul className="mt-6 flex flex-col gap-3">
         <li className="text-base flex gap-2 text-textDark">
           <span className="text-textOrange mt-1">
             <TiArrowForward />
           </span>
-          Ensuring computers and laptops run smoothly.
+          {'Ensuring computers and laptops run smoothly.'}
         </li>
         <li className="text-base flex gap-2 text-textDark">
           <span className="text-textOrange mt-1">
             <TiArrowForward />
           </span>
-          Adding website features as per supervisor's instructions
+          {'Adding website features as per supervisors instructions.'}
         </li>
         <li className="text-base flex gap-2 text-textDark">
           <span className="text-textOrange mt-1">
             <TiArrowForward />
           </span>
-          Repairing laptop and computer hardware and software
+          {'Repairing laptop and computer hardware and software.'}
         </li>
       </ul>
     </motion.div>
