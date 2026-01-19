22fb6833ec581c920e185d621b02141af3519908
RickyDarwid <63650667+rickydarmawanwidjanarko@users.noreply.github.com>
2026-01-19 11:07:43 +0800

    Update 3

--- components/works/AvillaBali.tsx
+++ components/works/AvillaBali.tsx
@@ -11,7 +11,7 @@ const AvillaBali = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        {'IT'}
+        {'IT Support Specialist'}
         <span className="text-textOrange tracking-wide">{'@AvillaBali'}</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
